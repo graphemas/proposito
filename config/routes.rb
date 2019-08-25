5e9e2baec803a68bcb8fb596e399183c3656fc25
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2019-08-24 20:05:13 -0500

    Sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/one'
+  get 'home/two'
+  get 'home/three'
+  get 'home/four'
   get '/service-worker.js' => "service_worker#service_worker"
   get '/manifest.json' => "service_worker#manifest"
   get '/offline.html' => "service_worker#offline"
