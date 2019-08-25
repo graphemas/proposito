3abb06959afae176d88d0347afb5b45ca0a2d025
Froy <ctidev04@gmail.com>
2019-08-25 03:13:31 -0500

    rad json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@
 class HomeController < ApplicationController
   
   def index
-    @data = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/brand/list")
+    @data = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/")
   end
 end
