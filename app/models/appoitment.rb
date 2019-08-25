446c12ef491c2989a83f8a6f26f32d487b5cf4f9
Froy <ctidev04@gmail.com>
2019-08-25 10:46:43 -0500

    add combo store

--- app/models/appoitment.rb
+++ app/models/appoitment.rb
@@ -1,2 +1,4 @@
 class Appoitment < ApplicationRecord
+  STORES=[["Elektra, Ciudad de México", "1"],["Elektra Tacuba 1 Ciudad de México","2"], ["Elektra Tacuba Miguel Schultz
+  Ciudad de México", "3"]]
 end
