31adeb000a566937250cf24931738f5d2b8bb55e
Froy <ctidev04@gmail.com>
2019-08-25 05:24:37 -0500

    add product detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
     def index
         @products = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/")
     end
+  
+  def show
+    id= params[:id]
+    @product = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/products/#{id}/specification")
+  end
 end
