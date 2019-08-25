e86c16ab354817268682b94fd670659c77608738
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2019-08-25 04:34:23 -0500

    List categories

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,14 +18,14 @@ import LocalTime from "local-time"
 LocalTime.start()
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faHome, faQuestion, faMehBlank, faShoppingBag, faBars, faCreditCard } from "@fortawesome/free-solid-svg-icons";
-library.add(faHome, faQuestion, faMehBlank, faShoppingBag, faBars, faCreditCard);
+import { faHome, faSearch, faMehBlank, faShoppingBag, faBars, faCreditCard } from "@fortawesome/free-solid-svg-icons";
+library.add(faHome, faSearch, faMehBlank, faShoppingBag, faBars, faCreditCard);
 dom.watch();
 
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js', { scope: './' })
     .then(function(reg) {
-      console.log('[Companion]', 'Service worker registered!');
+      console.log('[Vink]', 'Service worker registered!');
       console.log(reg);
     });
 }
