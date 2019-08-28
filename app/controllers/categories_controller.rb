fbab6b1dd778d65c243096274f4e024b0f77f761
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2019-08-28 14:34:52 -0500

    Fixed api path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/")
+    #https://elektra.vteximg.com.br/api/catalog_system/pub/category/tree/3
+    #https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/
+    @categories = gt_data("https://elektra.vteximg.com.br/api/catalog_system/pub/category/tree/3")
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@
 class HomeController < ApplicationController
   
   def index
-    @data = gt_data("https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/")
+    #https://elektra.vteximg.com.br/api/catalog_system/pub/category/tree/3
+    #https://challenge.ektdevelopers.com/api/catalog_system/pvt/category/tree/320/
+    @data = gt_data("https://elektra.vteximg.com.br/api/catalog_system/pub/category/tree/3")
   end
 end
